ca38c0d2defd57cfa687e067aa4b1007fc87c8ac
EwanNoble <e.noble@live.co.uk>
2018-12-06 21:31:12 +0000

    :construction: WIP Auth functions

--- Tests/QT002.Structure.Tests.ps1
+++ Tests/QT002.Structure.Tests.ps1
@@ -8,7 +8,6 @@ Describe "Code structure tests" -Tag "Structure" {
             It "Should have a correctly named Creator script" {
                 Get-ChildItem -Path "$($Provider.FullName)/Start-$($Provider.Name)RepositoryCreator.ps1"
             }
-
         }
     }
 
@@ -20,7 +19,6 @@ Describe "Code structure tests" -Tag "Structure" {
             It "Should have a correctly named Creator script" {
                 Get-ChildItem -Path "$($Provider.FullName)/Start-$($Provider.Name)CICreator.ps1"
             }
-
         }
     }
 
@@ -32,7 +30,20 @@ Describe "Code structure tests" -Tag "Structure" {
             It "Should have a correctly named Creator script" {
                 Get-ChildItem -Path "$($Provider.FullName)/Start-$($Provider.Name)CDCreator.ps1"
             }
+        }
+    }
 
+    Context "All Providers have an Auth function" {
+
+        $Providers = @((Get-ChildItem -Path "$PSScriptRoot/.." -Directory | Where-Object { $_.Name -match "Repository|CI|CD" } | Get-ChildItem -Directory).Name) |
+            Sort-Object -Unique
+
+        $AuthFunctions = @((Get-ChildItem -Path "$PSScriptRoot/../Auth/*.ps1").Name)
+
+        foreach ($Provider in $Providers) {
+            It "$Provider has an Auth function" {
+                ($AuthFunctions -contains "Initialise-$($Provider)Provider.ps1") | Should Be $true
+            }
         }
     }
 }
